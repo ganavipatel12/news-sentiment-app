004372e29a31a9b1ed019a2bbe6054e5745cb5ee
Ganavi H Patel <ganavipatel2002@gmail.com>
2025-03-23 22:17:01 +0800

    Deploying to Hugging Face

--- api.py
+++ api.py
@@ -35,6 +35,8 @@ def analyze():
         "sentiment_report": sentiment_report
     })
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+#if __name__ == "__main__":
+    #app.run(debug=True, port=5000)
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=7860)
